8d947ff5cabb2371e7a9844e4bffb23e8b632461
Lionel Tzatzkin <elrumordelaluz@hotmail.com>
2020-08-25 10:02:15 +0200

    Fixes

--- api/index.js
+++ api/index.js
@@ -2,23 +2,26 @@ const svgSpreact = require('svg-spreact')
 
 module.exports = async (req, res) => {
   if (req.method === 'OPTIONS') {
-    return res.send('ok!')
+    return res.status(200).send('ok!')
   }
   const { input, names, tidy, optimize, className } = await req.body
   if (input) {
     try {
-      return await svgSpreact(input, {
+      const sprite = await svgSpreact(input, {
         ...(tidy ? { tidy: tidy === 'false' ? false : true } : {}),
         ...(optimize ? { optimize: optimize === 'false' ? false : true } : {}),
         ...(names ? { processId: (n) => names[n] || `Icon_${n}` } : {}),
         ...(className ? { className } : {}),
       })
+      res.status(200).send(sprite)
     } catch (err) {
-      return res.send(`Error: ${err}`)
+      return res.status(400).send(`Error: ${err}`)
     }
   } else {
-    return res.send(
-      '<p style="display:flex;align-items:center;justify-content:center;height:100vh;"><a style="text-decoration:none;color:inherit;font-family:sans-serif;" href="https://elrumordelaluz.github.io/micro-svg-spreact/">visit svg-spreact</a></p>'
-    )
+    return res
+      .status(200)
+      .send(
+        '<p style="display:flex;align-items:center;justify-content:center;height:100vh;"><a style="text-decoration:none;color:inherit;font-family:sans-serif;" href="https://elrumordelaluz.github.io/micro-svg-spreact/">visit svg-spreact</a></p>'
+      )
   }
 }
