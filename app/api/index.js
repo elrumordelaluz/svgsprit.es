8673f672a51498ca598920ef8084dfe70bee05ec
Lionel Tzatzkin <elrumordelaluz@hotmail.com>
2020-08-25 10:51:07 +0200

    Allow cors

--- app/api/index.js
+++ app/api/index.js
@@ -1,6 +1,26 @@
 const svgSpreact = require('svg-spreact')
 
-module.exports = async (req, res) => {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = async (req, res) => {
   if (req.method === 'OPTIONS') {
     return res.status(200).send('ok!')
   }
@@ -25,3 +45,5 @@ module.exports = async (req, res) => {
       )
   }
 }
+
+module.exports = allowCors(handler)
