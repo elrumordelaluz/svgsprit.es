7428e6c5e9f68cf5e360010e710ab37b80df3c65
Lionel <elrumordelaluz@hotmail.com>
2022-11-09 12:59:09 +0100

    Add @vercel/analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
+import { Analytics } from '@vercel/analytics/react'
 import '../styles.css'
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
