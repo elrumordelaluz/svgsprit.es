64485f8db854477b7a62034f4af2021287d9b4c9
Lionel <elrumordelaluz@hotmail.com>
2023-06-09 08:32:09 +0200

    Remove @vercel/analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
-import { Analytics } from '@vercel/analytics/react'
 import '../styles.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  )
+  return <Component {...pageProps} />
 }
