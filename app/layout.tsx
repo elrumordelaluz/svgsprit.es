43c8bd7a7e7f35210c7ccde8383f4a9377a098b7
Lionel <elrumordelaluz@hotmail.com>
2024-03-21 13:51:47 +0100

    Add missing GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import { cn } from "@/lib/utils"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import { cn } from '@/lib/utils'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
 export const metadata: Metadata = {
-  title: "SVG Sprites Generator",
-  description: "Public endpoint to generate SVG Sprites using svg-spreact",
+  title: 'SVG Sprites Generator',
+  description: 'Public endpoint to generate SVG Sprites using svg-spreact',
 }
 
 export default function RootLayout({
@@ -19,12 +20,13 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable,
+          'min-h-screen bg-background font-sans antialiased',
+          inter.variable
         )}
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-0WE8F1DXK7" />
     </html>
   )
 }
